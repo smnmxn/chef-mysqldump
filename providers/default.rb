6b3c339d54a5f45f1f7cf6f1acae75dd8fb0df11
Simon Moxon <simon.moxon@meetupcall.com>
2014-01-13 13:06:43 +0000

    Migrated from definition to full-on LWRP to support notifies

--- providers/default.rb
+++ providers/default.rb
@@ -1,10 +1,13 @@
 
-define :mysqldump, :action => :enable, :dbhost => 'localhost', :dbname => nil, :overwrite => false do
-  include_recipe 'mysql::client'
-  
-  if params[:action] == :enable
+def whyrun_supported?
+  true
+end
+
+use_inline_resources if defined?(use_inline_resources)
+
+action :run do
 	  unless File.exist?(params[:name]) && params[:overwrite] == false
 	    execute "mysqldump -h #{params[:dbhost]} -u #{params[:dbuser]} -p#{params[:dbpassword]} #{params[:dbname]} > #{params[:name]}"
+	    new_resource.updated_by_last_action(true)
 	  end
-	end
 end
---
+++ resources/default.rb
@@ -0,0 +1,8 @@
+actions :run
+default_action :run
+
+attribute :dbhost,        :kind_of => String
+attribute :dbuser,        :kind_of => String
+attribute :dbpassword,    :kind_of => String
+attribute :dbname,        :kind_of => String
+
